783fbc675d6291bf643e78f661ad0332362dcc92
Artur Wenc <artur.wenc.dev@gmail.com>
2020-09-14 19:07:17 +0100

    Implemented database and adjusted methods to use database.

--- main.py
+++ main.py
@@ -20,30 +20,54 @@ meme_put_args = reqparse.RequestParser()
 meme_put_args.add_argument("name", type=str, help="Name of meme")
 meme_put_args.add_argument("likes", type=int, help="likes of meme")
 
-memes = {}
+meme_update_args = reqparse.RequestParser()
+meme_update_args.add_argument("name", type=str, help="Name of meme")
+meme_update_args.add_argument('likes', type=int, help='Number of likes of meme')
 
-def abort_if_no_id(meme_id):
-    if meme_id not in memes:
-        abort(404, message="Meme ID do not exists")
+resource_field = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'likes': fields.Integer
+}
 
-def abort_if_meme(meme_id):
-    if meme_id in memes:
-        abort(409, message=f'Meme with {meme_id} already exists')
 class Meme(Resource):
+    @marshal_with(resource_field)
     def get(self, meme_id):
-        abort_if_no_id(meme_id)
-        return memes[meme_id]
+        result = MemeModel.query.filter_by(id=meme_id).first()
+        if not result:
+            abort(404, message='Could not find ID')
+        return result
 
+    @marshal_with(resource_field)
     def put(self, meme_id):
-        abort_if_meme(meme_id)
         args = meme_put_args.parse_args()
-        memes[meme_id] = args
-        return memes[meme_id], 201
+        result = MemeModel.query.filter_by(id=meme_id).first()
+        if result:
+            abort(409, message="ID taken!")
+        meme = MemeModel(id=meme_id, name=args['name'], likes=args['likes'])
+        db.session.add(meme)
+        db.session.commit()
+        return meme, 201
+
+    def patch(self, meme_id):
+        args = meme_update_args.parse_args()
+        result = MemeModel.query.filter_by(id=meme_id).first()
+        if not result:
+            abort(404, message='Meme ID dont exist')
+        if 'name' in args and args['name'] is not None:
+            result.name = args['name']
+        if args['likes']:
+            result.likes = args['likes']
+        db.session.commit()
+        return result
 
     def delete(self, meme_id):
-        abort_if_no_id(meme_id)
-        del memes[meme_id]
-        return '', 204
+        meme = MemeModel.query.filter_by()
+        if meme:
+            db.session.delete(meme)
+            db.session.commit()
+        return f'Meme ID {meme_id} deleted', 204
+
 
 api.add_resource(Meme, '/meme/<int:meme_id>')
 
