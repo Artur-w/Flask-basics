65847985aec4ca8304acb169d6b94e5df34b8071
Artur Wenc <artur.wenc.dev@gmail.com>
2021-08-18 12:59:12 +0100

    adding variable rules

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ app = Flask(__name__) # create an instance of the class
 def welcome():
     return "Hello World!"
 
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is " + str(number+1)
+
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello " + name
+
+
+
 # __name__ special variable in Python - takes the value of the script name
 # it make sure the app runs only when executed in main file and not when it is imported in some other file.
 if __name__ == '__main__':
